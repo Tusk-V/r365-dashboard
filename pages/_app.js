120826936981b611f98580192cc4e8cbe705431a
Tusk-V <164945879+Tusk-V@users.noreply.github.com>
2026-05-22 09:41:33 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,16 @@
 import { SessionProvider } from "next-auth/react"
+import Head from "next/head"
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <SessionProvider session={session}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, viewport-fit=cover"
+        />
+      </Head>
       <Component {...pageProps} />
     </SessionProvider>
   )
